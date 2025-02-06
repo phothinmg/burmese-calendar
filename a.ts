21bf23d61a19dd986730ca90985688fbccefaebf
phothinmg <phothinmg@gmail.com>
2025-02-06 07:00:01 +0630

    deleted:    a.ts

--- a.ts
+++
@@ -1,9 +0,0 @@
-import { Calendar, blankCellBefore } from "./src/index.js";
-import fs from "node:fs";
-const cal = new Calendar();
-
-const mv = cal.monthView({ year: 2025, month: 2, lang: "Burmese" });
-
-//fs.writeFileSync("aa.json", JSON.stringify(mv, null, 2));
-
-console.log(blankCellBefore(3));
