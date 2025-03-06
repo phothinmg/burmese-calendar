c721f0ef2e2896624cfdbfdb0d32ca30702de163
phothinmg <phothinmg@gmail.com>
2025-03-06 16:08:22 +0630

    modified:   index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -9,28 +9,83 @@ declare namespace Bcal {
     English,
     Burmnese,
   }
-
+  /**
+   * Calendar Types
+   */
   enum Ct {
     gregorian,
     julian,
   }
 
   type BcalInfo = {
+    /**
+     * Sasana Year [BE]
+     */
     sasana_year: number;
+    /**
+     * Sasana Year [BE]
+     * If translate to Burmese.
+     */
     sasana_year_str: string;
+    /**
+     * Burmese Year [ME]
+     */
     burmese_year: number;
+    /**
+     * Burmese Year [ME]
+     * If translate to Burmese.
+     */
     burmese_year_str: string;
+    /**
+     * Burmese month index
+     * [
+     *  Tagu=1, Kason=2, Nayon=3, 1st Waso=0, (2nd) Waso=4, Wagaung=5,
+     *  Tawthalin=6, Thadingyut=7, Tazaungmon=8, Nadaw=9, Pyatho=10, Tabodwe=11,
+     *  Tabaung=12, Late Tagu=13, Late Kason=14
+     * ]
+     */
     burmese_month_index: number;
+    /**
+     * Burmese month
+     */
     burmese_month_str: string;
+    /**
+     * Moon Phases index
+     * [0=waxing, 1=full moon, 2=waning, 3=new moon]
+     */
     moon_phases_index: number;
+    /**
+     * Moon Phase String
+     */
     moon_phases_str: string;
+    /**
+     * Fortnight Day [1-15]
+     */
     fortnight_day: number;
+    /**
+     * Fortnight Day String
+     */
     fortnight_day_str: string;
+    /**
+     * Burmese Day [1-30]
+     */
     burmese_day: number;
+    /**
+     * Burmese Day String
+     */
     burmese_day_str: string;
+    /**
+     * Mahabote
+     */
     mahabote: string;
+    /**
+     * Heading of Dragon 
+     */
     nagahle: string;
     nakhat: string;
+    /**
+     * For Buddhist Lent,sabbath days are school holiday else false.
+     */
     is_sabbath_schoolHoliday: boolean;
     sabbath: string;
     yatyaza: string;
@@ -81,10 +136,10 @@ declare namespace Bcal {
   /**
    * @description
    * Convert Gregorian Date and Time to Julian Date and Julian Day Number.
-   * @param {number} year
-   * @param {number} month
-   * @param {number} day
-   * @param {number} [hour=12]
+   * @param {number} year The year of the date.
+   * @param {number} month The month of the date [1-12].
+   * @param {number} day  The day of the month [1-31].
+   * @param {number} [hour=12] Hour default 12
    * @param {number} [minute=0]
    * @param {number} [seconds=0]
    * @param {number} [tz_offset=0.0]
@@ -164,4 +219,38 @@ declare namespace Bcal {
     month: number,
     day: number
   ): { year: number; month: number; day: number };
+
+  /**
+   * @description
+   * Retrieve a DateView object containing detailed information about a specific date.
+   * @param {number} year - The year of the date.
+   * @param {number} month - The month of the date (1-12).
+   * @param {number} day - The day of the month.
+   * @param {Languages} [lang] - Optional language parameter to specify the language of the date information.
+   * @returns {DateView} An object containing the Gregorian and Burmese calendar details for the specified date.
+   */
+  function dateView(
+    year: number,
+    month: number,
+    day: number,
+    lang?: Languages
+  ): DateView;
+
+  /**
+   * @description
+   * Retrieve a MonthView object containing detailed information about a specific date.
+   * @param {number} year - The year of the date.
+   * @param {number} month - The month of the date (1-12).
+   * @param {Languages} [lang] - Optional language parameter to specify the language of the date information.
+   * @returns {MonthView} An object containing the Gregorian and Burmese calendar details for the specified date.
+   */
+  function monthView(year: number, month: number, lang?: Languages): MonthView;
+  /**
+   * @description
+   * Retrieve a YearView object containing detailed information about a specific date.
+   * @param {number} [year] - The year of the date.Optional default - current year.
+   * @param {Languages} [lang] - Optional language parameter to specify the language of the date information.
+   * @returns {YearView} An object containing the Gregorian and Burmese calendar details for the specified date.
+   */
+  function yearView(year?: number, lang?: Languages): YearView;
 }
